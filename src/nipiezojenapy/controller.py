d7020a5cb9cc99b960ef6d484eab0b0a958ef89f
G Adam Cox <gadamc@gmail.com>
2022-08-24 16:23:12 -0700

    adds get current voltage function.

--- src/nipiezojenapy/controller.py
+++ src/nipiezojenapy/controller.py
@@ -18,6 +18,7 @@ class PiezoControl:
         self.last_write_values = [-1, -1, -1]
         self.minimum_allowed_position = 0
         self.maximum_allowed_position = 80
+        self.settling_time_in_seconds = 0.400
 
     def _microns_to_volts(self, microns: float) -> float:
         return microns / self.scale_microns_per_volt
@@ -44,7 +45,7 @@ class PiezoControl:
 
         raises ValueError if try to set position out of bounds.
         '''
-
+        logging.info(f'go to position {x}, {y}, {z}')
         if x:
             self._validate_value(x)
             with nidaqmx.Task() as task:
@@ -66,7 +67,8 @@ class PiezoControl:
                 task.write(self._microns_to_volts(z))
                 self.last_write_values[2] = z
 
-        time.sleep(0.1) #wait 100 milliseconds to ensure piezo actuator has settled into position.
+        time.sleep(self.settling_time_in_seconds) #wait to ensure piezo actuator has settled into position.
+        logging.info(f'last write: {self.last_write_values}')
 
     def step(self, dx: float = None,
                    dy: float = None,
@@ -75,7 +77,7 @@ class PiezoControl:
         Step a small amount in any direction.
 
         You do not need to specify all three axis values in order
-        to move in one direction. For example, you can call: step(dz = 0.1)
+        to move in one direction. For example, you can call: step(dz = 0.5)
         '''
         x, y, z = self.get_current_position()
 
@@ -98,20 +100,34 @@ class PiezoControl:
     def get_current_position(self) -> list[float]:
         '''
         Returns the x,y,z position in microns
+
+        If no input analog channels were provided when objected was created,
+        returns the last requested position.
         '''
 
         if self.read_channels is None:
             return self.last_write_values
 
+        else:
+            return [self._volts_to_microns(v) for v in self.get_current_voltage()]
+
+    def get_current_voltage(self) -> list[float]:
+        '''
+        Returns the voltage supplied to the three input analog channels.
+
+        If no input analog channels were provided when objected was created,
+        returns [-1,-1,-1]
+        '''
         output = [-1,-1,-1]
-        with nidaqmx.Task('xr') as xread, nidaqmx.Task('yr') as yread, nidaqmx.Task('zr') as zread:
+        if self.read_channels is not None:
+            with nidaqmx.Task('xr') as xread, nidaqmx.Task('yr') as yread, nidaqmx.Task('zr') as zread:
 
-             xread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[0])
-             yread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[1])
-             zread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[2])
+                 xread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[0])
+                 yread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[1])
+                 zread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[2])
 
-             output[0] = self._volts_to_microns(xread.read())
-             output[1] = self._volts_to_microns(yread.read())
-             output[2] = self._volts_to_microns(zread.read())
+                 output[0] = xread.read()
+                 output[1] = yread.read()
+                 output[2] = zread.read()
 
         return output
