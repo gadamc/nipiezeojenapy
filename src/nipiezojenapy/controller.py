4be224f3f7ebf4b2863f90dec838fbc42966885a
G Adam Cox <gadamc@gmail.com>
2022-09-01 17:26:11 -0700

    Adds better hardware range checks Adds logging info statements Improves nidaqmx task creation, trying to avoid some errors.

--- src/nipiezojenapy/controller.py
+++ src/nipiezojenapy/controller.py
@@ -15,10 +15,10 @@ class PiezoControl:
         self.write_channels = write_channels
         self.read_channels = read_channels
         self.scale_microns_per_volt = scale_microns_per_volt
-        self.last_write_values = [-1, -1, -1]
-        self.minimum_allowed_position = 0
-        self.maximum_allowed_position = 80
-        self.settling_time_in_seconds = 0.400
+        self.last_write_values = [None, None, None]
+        self.minimum_allowed_position = 0.01
+        self.maximum_allowed_position = 40.02
+        self.settling_time_in_seconds = 0.200
 
     def _microns_to_volts(self, microns: float) -> float:
         return microns / self.scale_microns_per_volt
@@ -27,12 +27,20 @@ class PiezoControl:
         return  self.scale_microns_per_volt * volts
 
     def _validate_value(self, position: float) -> None:
+        position = float(position)
         if type(position) not in [type(1.0), type(1)]:
             raise TypeError(f'value {position} is not a valid type.')
         if position < self.minimum_allowed_position:
-            raise ValueError(f'value {position} is less than zero.')
+            raise ValueError(f'value {position} is less than {self.minimum_allowed_position:.2f}.')
         if position > self.maximum_allowed_position:
-            raise ValueError(f'value {position} is greater than 80.')
+            raise ValueError(f'value {position} is greater than {self.maximum_allowed_position:.2f}.')
+
+    def check_allowed_position(self, x: float = None,
+                                     y: float = None,
+                                     z: float = None) -> None:
+        if x is not None: self._validate_value(x)
+        if y is not None: self._validate_value(y)
+        if z is not None: self._validate_value(z)
 
     def go_to_position(self, x: float = None,
                              y: float = None,
@@ -45,30 +53,29 @@ class PiezoControl:
 
         raises ValueError if try to set position out of bounds.
         '''
-        logging.info(f'go to position {x}, {y}, {z}')
-        if x:
-            self._validate_value(x)
-            with nidaqmx.Task() as task:
-                task.ao_channels.add_ao_voltage_chan(self.device_name + '/' + self.write_channels[0])
-                task.write(self._microns_to_volts(x))
-                self.last_write_values[0] = x
 
-        if y:
-            self._validate_value(y)
+        def goto(val, idx):
+            self._validate_value(val)
             with nidaqmx.Task() as task:
-                task.ao_channels.add_ao_voltage_chan(self.device_name + '/' + self.write_channels[1])
-                task.write(self._microns_to_volts(y))
-                self.last_write_values[1] = y
+                task.ao_channels.add_ao_voltage_chan(self.device_name + '/' + self.write_channels[idx])
+                task.write(self._microns_to_volts(val))
+                self.last_write_values[idx] = val
 
-        if z:
-            self._validate_value(z)
-            with nidaqmx.Task() as task:
-                task.ao_channels.add_ao_voltage_chan(self.device_name + '/' + self.write_channels[2])
-                task.write(self._microns_to_volts(z))
-                self.last_write_values[2] = z
+        debug_string = []
+        if x is not None:
+            goto(x, 0)
+            debug_string.append(f'x: {x:.2f}')
+        if y is not None:
+            goto(y, 1)
+            debug_string.append(f'y: {y:.2f}')
+        if z is not None:
+            goto(z, 2)
+            debug_string.append(f'z: {z:.2f}')
+
+        logger.info(f'go to position {" ".join(debug_string)}')
 
         time.sleep(self.settling_time_in_seconds) #wait to ensure piezo actuator has settled into position.
-        logging.info(f'last write: {self.last_write_values}')
+        logger.debug(f'last write: {self.last_write_values}')
 
     def step(self, dx: float = None,
                    dy: float = None,
@@ -85,17 +92,17 @@ class PiezoControl:
             try:
                 self.go_to_position(x = x + dx)
             except ValueError as e:
-                logger.warning('Trying to step outside of allowed range (0, 80).')
+                logger.error(f'Trying to step outside of allowed range ({self.minimum_allowed_position:.2f}, {self.maximum_allowed_position:.2f}).')
         if dy:
             try:
                 self.go_to_position(y = y + dy)
             except ValueError as e:
-                logger.warning('Trying to step outside of allowed range (0, 80).')
+                logger.error(f'Trying to step outside of allowed range ({self.minimum_allowed_position:.2f}, {self.maximum_allowed_position:.2f}).')
         if dz:
             try:
                 self.go_to_position(z = z + dz)
             except ValueError as e:
-                logger.warning('Trying to step outside of allowed range (0, 80).')
+                logger.error(f'Trying to step outside of allowed range ({self.minimum_allowed_position:.2f}, {self.maximum_allowed_position:.2f}).')
 
     def get_current_position(self) -> list[float]:
         '''
@@ -120,7 +127,7 @@ class PiezoControl:
         '''
         output = [-1,-1,-1]
         if self.read_channels is not None:
-            with nidaqmx.Task('xr') as xread, nidaqmx.Task('yr') as yread, nidaqmx.Task('zr') as zread:
+            with nidaqmx.Task() as xread, nidaqmx.Task() as yread, nidaqmx.Task() as zread:
 
                  xread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[0])
                  yread.ai_channels.add_ai_voltage_chan(self.device_name + '/' + self.read_channels[1])
